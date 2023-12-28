407e6229f52375f4bde5633b07aa7e70a48b1ab6
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-12-29 07:48:14 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module NextPlay
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = [:en, :ja]
+    config.time_zone = "Tokyo"
   end
 end
