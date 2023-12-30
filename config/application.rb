4f0a51579c35a4b4accf0bad76622423b8becef7
sakusaku3939 <yuukiyuuki223@gmail.com>
2023-12-30 21:55:25 +0900

    タイムゾーンをUTCに変更

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module NextPlay
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:en, :ja]
-    config.time_zone = "Tokyo"
+    config.time_zone = "UTC"  # ActiveRecordのタイムゾーンと統一するために、UTCのままにする
   end
 end
