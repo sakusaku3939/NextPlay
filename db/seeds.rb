581f28c314b049916c15775d2a9d82aef2e42f6a
sakusaku3939 <yuukiyuuki223@gmail.com>
2024-01-13 19:38:02 +0900

    初期データをさらに追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,20 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+User.create(email: "1@example.com", password: "test1111")
+User.create(email: "2@example.com", password: "test2222")
+User.create(email: "3@example.com", password: "test3333")
+User.create(email: "4@example.com", password: "test4444")
+User.create(email: "5@example.com", password: "test5555")
+User.create(email: "6@example.com", password: "test6666")
+
 Profile.create(user_id: 1, username: "bootstrap", nickname: "Bootstrap")
 Profile.create(user_id: 2, username: "stub_", nickname: "Stub")
 Profile.create(user_id: 3, username: "ice_protocol", nickname: "ice_protocol")
 Profile.create(user_id: 4, username: "pyopyo", nickname: "piyopiyo")
 Profile.create(user_id: 5, username: "foo", nickname: "foo")
 Profile.create(user_id: 6, username: "username", nickname: "HogeHoge")
+Profile.create(user_id: 7, username: "anonymous", nickname: "Anonymous")
 
 Post.create(profile_id: 1, content: "valorant枠", stream_id: 1)
 Post.create(profile_id: 2, content: "誰かスト6対戦しましょう")
